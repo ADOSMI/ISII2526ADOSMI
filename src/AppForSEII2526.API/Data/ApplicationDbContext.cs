60dc90a44501182047d10646ab1a769e694f9b30
Adolfo-em <127391439+Adolfo-em@users.noreply.github.com>
2025-10-05 13:45:21 +0200

    He creado todas las clases relacionadas a mi caso de uso Comprar Bono de Bocadillos, el diagrama de clases de uso y he creado el ApplicationDbContext y el ApplicationUser. (#11)
    Co-authored-by: adolfo-em <Adolfojose.escribano@alu.uclm>

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -4,5 +4,31 @@ using AppForSEII2526.API.Models;
 
 namespace AppForSEII2526.API.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<BonoBocadillo> BonoBocadillo { get; set; }
+    public DbSet<BonosComprados> BonosComprados { get; set; }
+    public DbSet<CompraBono> CompraBono { get; set; }
+    public DbSet<TipoBocadillo> TipoBocadillo { get; set; }
+
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+
+        base.OnModelCreating(builder);
+
+        //CLAVES ALTERNATIVAS
+        builder.Entity<BonosComprados>()
+            .HasAlternateKey(bc => new { bc.Id, bc.IdCompra });
+
+
+
+
+    }
 }
---
+++ src/AppForSEII2526.API/Models/BonosComprados.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class BonosComprados
+{
+	public BonosComprados()
+	{
+	}
+	public BonosComprados(int bonoCompradosId, int cantidad, int idCompra, double precioBono, BonoBocadillo bonoBocadillo, CompraBono compraBono)
+    {
+        Id = bonoCompradosId;
+        IdCompra = idCompra;
+        Cantidad = cantidad;
+        PrecioBono = precioBono;
+        BonoBocadillo = bonoBocadillo;
+        CompraBono = compraBono;
+    }
+
+    //BONO COMPRADOS ID
+    [Key] //Clave Primaria
+    public int Id { get; set; }
+
+    //CANTIDAD COMPRA
+    [Required]
+    [Display(Name = "Cantidad de Compra")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad mínima de compra es 1")]
+    public int Cantidad { get; set; }
+
+    //ID COMPRA
+    public int IdCompra { get; set; }
+
+    //PRECIO BONO
+    [Required]
+    [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+    [Range(1, float.MaxValue, ErrorMessage = "Precio mínimo es 1")]
+    [Display(Name = "Precio")]
+    public double PrecioBono { get; set; }
+
+    //BONO BOCADILLO 
+    [Required]  //Requerido porque es 1 a N
+    public BonoBocadillo BonoBocadillo { get; set; }
+
+    //COMPRA BONO
+    [Required] //Requerido porque es 1 a N
+    public CompraBono CompraBono { get; set; }
+
+}
