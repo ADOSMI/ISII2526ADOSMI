6b4d113ccb8414cb37c75bce59455089b7534467
adolfo-em <Adolfojose.escribano@alu.uclm>
2025-10-05 13:26:32 +0200

    He creado todas las clases relacionadas a mi caso de uso Comprar Bono de Bocadillos, el diagrama de clases de uso y he creado el ApplicationDbContext y el ApplicationUser.

---
+++ src/AppForSEII2526.API/Models/BonoBocadillo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class BonoBocadillo
+{
+	public BonoBocadillo()
+	{
+	}
+
+	public BonoBocadillo(TipoBocadillo tipobocadillo, int bonoBocadilloId, int cantidadDisponible, int nBocadillos, string nombre, double PVP, IList<BonosComprados> bonosComprados)
+	{
+        TipoBocadillo tipoBocadillo;
+		Id = bonoBocadilloId;
+        CantidadDisponible = cantidadDisponible;
+        NumeroBocadillos = nBocadillos;
+        Nombre = nombre;
+        PrecioPorBono = PVP;
+        BonosComprados = bonosComprados;
+
+    }
+
+    //TIPO BOCADILLO 
+    [Required]  //Requerido porque es 1 a N
+    public TipoBocadillo TipoBocadillo  { get; set; }
+
+    //BONO BOCADILLO ID
+    [Key] //Clave Primaria
+    public int Id { get; set; }
+
+    //CANTIDAD DISPONIBLE
+    [Required]
+    [Display(Name = "Cantidad disponible")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad disponible mínima es 1")]
+    public int CantidadDisponible { get; set; }
+
+    //NUMERO DE BOCADILLOS POR BONO
+    [Required]
+    [Display(Name = "Numero de bocadillos por bono")]
+    [Range(1, int.MaxValue, ErrorMessage = "El número mínimo de bocadillos por bono es 1")]
+    public int NumeroBocadillos { get; set; }
+
+    public string Nombre { get; set; }
+
+    //PRECIO POR BONO
+    [Required]
+    [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+    [Range(1, float.MaxValue, ErrorMessage = "Precio mínimo es 1")]
+    [Display(Name = "Precio Por Bono")]
+    public double PrecioPorBono { get; set; }
+
+    public IList<BonosComprados> BonosComprados { get; set; }
+
+
+}
---
+++ src/AppForSEII2526.API/Models/CompraBono.cs
@@ -0,0 +1,82 @@
+﻿
+
+namespace AppForSEII2526.API.Models
+{
+
+    public class CompraBono
+    {
+	    public CompraBono()
+	    {
+	    }
+
+        public CompraBono(string apellidoBono1, string apellidoBono2, int compraBonoId, DateTime fechaCompraBono, EnumMetodosPago metodoPago, int nBonos, string nombreCliente, double precioTotalBono, IList<BonosComprados> bonosComprados)
+        {
+            ApellidoBono1 = apellidoBono1;
+            ApellidoBono2 = apellidoBono2;
+            Id = compraBonoId;
+            FechaCompra = fechaCompraBono;
+            MetodoPago = metodoPago;
+            NBonos = nBonos;
+            NombreCliente = nombreCliente;
+            PrecioTotalBono = precioTotalBono;
+            BonosComprados = bonosComprados;
+        }
+
+
+        //APELLIDO BONO 1
+        [Display(Name = "Primer Apellido")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su primer apellido")]
+        [StringLength(40, ErrorMessage = "Primer Apellido no puede ser mayor de 40 caracteres")]
+        public string ApellidoBono1 { get; set; }
+
+        //APELLIDO BONO 2
+        [Display(Name = "Segundo Apellido")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su segundo apellido")]
+        [StringLength(40, ErrorMessage = "Segundo Apellido no puede ser mayor de 40 caracteres")]
+        public string ApellidoBono2 { get; set; }
+    
+        //COMPRA BONO ID
+        [Key] //Clave Primaria
+        public int Id { get; set; }
+
+        //FECHA DE COMPRA
+        [Required, DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de compra")]
+        public DateTime FechaCompra { get; set; }
+
+        //METODO DE PAGO
+        [Display(Name = "Método de pago")]
+        [Required(ErrorMessage = "Elija un método de pago")]
+        public EnumMetodosPago MetodoPago { get; set; }
+
+        //NÚMERO DE BONOS
+        [Display(Name = "Numero de bonos")]
+        [Range(1, int.MaxValue, ErrorMessage = "Numero de bonos mínimo es 1")]
+        public int NBonos { get; set; }
+
+        //NOMBRE CLIENTE
+        [Display(Name = "Nombre")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, introduzca su nombre")]
+        [StringLength(40, ErrorMessage = "Nombre no puede ser mayor de 40 caracteres")]
+        public string NombreCliente { get; set; }
+
+        //PRECIO TOTAL BONO
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "Precio mínimo es 1")]
+        [Display(Name = "Precio Total Bono")]
+        public double PrecioTotalBono { get; set; }
+
+        public IList<BonosComprados> BonosComprados { get; set; }
+
+
+        public enum EnumMetodosPago
+        {
+            TarjetaCredito,
+            PayPal,
+            GooglePay
+        }
+    }
+
+
+    
+}
