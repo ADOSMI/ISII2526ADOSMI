d89d93f333b475d282954d5546a8f94c1cf7d139
MIGUEL DE HARO GONZÁLEZ <Miguel.Haro1@alu.uclm.es>
2025-10-06 19:03:35 +0200

    He creado todas las clases del caso de Uso Comprar Merch y he añadido mi parte del DbContext

---
+++ src/AppForSEII2526.API/Models/Producto.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace AppForSEII2526.API.Models
+{
+    public class Producto
+    {
+        public Producto() { }
+
+        public Producto(int productoId, string nombre, double pvp, int stock, int tipoProductoId)
+        {
+            ProductoId = productoId;
+            Nombre = nombre;
+            PVP = pvp;
+            Stock = stock;
+            
+        }
+
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "El nombre del producto no puede contener más de 50 caracteres.")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "Precio mínimo es 1")]
+        [Display(Name = "Precio")]
+        public double PVP { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "El stock no puede ser negativo.")]
+        public int Stock { get; set; }
+
+       
+
+        // Relación con Producto_Compra
+        public IList<Producto_Compra> producto_Compras { get; set; } = new List<Producto_Compra>();
+    }
+}
---
+++ src/AppForSEII2526.API/Models/Producto_Compra.cs
@@ -0,0 +1,40 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(ProductoId), nameof(CompraId))]
+    public class Producto_Compra
+    {
+        public Producto_Compra() { }
+        public Producto_Compra(int cantidad, int compraid, int productoid, int pvp)
+        {
+            Cantidad = cantidad;
+            CompraId = compraid;
+            ProductoId = productoid;
+            PVP = pvp;
+           
+        }
+        [Key]
+        public int CompraId { get; set; }
+        //[ForeignKey("Producto")] ??
+        public int ProductoId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad mínima de compra es 1")]
+        public int Cantidad { get; set; }
+
+        // Precio unitario en el momento de la compra
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+        [Range(1, float.MaxValue, ErrorMessage = "Precio mínimo es 1")]
+        [Display(Name = "Precio unitario")]
+        public double PVP { get; set; }
+
+        [ForeignKey("CompraId")]
+        public int CompraId1 { get; set; }
+
+        [ForeignKey ("ResenyaId")]
+        public Resenya Resenya { get; set; }
+
+        public Compra_Producto compra { get; set; }
+        public Producto producto { get; set; }
+    }
+}
