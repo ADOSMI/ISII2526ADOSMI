0c4a5f902f19b87cb0362894a3dbe134043618fb
MIGUEL DE HARO GONZÁLEZ <Miguel.Haro1@alu.uclm.es>
2025-11-16 20:33:56 +0100

    DTOs y Controladores del metodo Details de mi caso de uso ComprarMerch

---
+++ src/AppForSEII2526.API/Controllers/MerchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MerchController : ControllerBase
+    {
+    }
+}
