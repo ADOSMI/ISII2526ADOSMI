251786146b962ed3fc20eaf8b5e60100cf42e32d
migueldharo <miguel.haro1@alu.uclm.es>
2025-11-16 22:33:59 +0100

    Ramas creadas (#99)

---
+++ src/AppForSEII2526.API/Data/dbo.Compra_Producto.data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[Compra_Producto] ON
+INSERT INTO [dbo].[Compra_Producto] ([Compraid], [Nombre], [DireccionEnvio], [FechaCompra], [Metodo_Pago], [PrecioFinal], [Apellido_1], [Apellido_2]) VALUES (1, N'Lucía', N'Calle del Sol 23', N'2025-11-16 00:00:00', N'Tarjeta de crédito', 92, N'Fernandéz', N'Pérez')
+SET IDENTITY_INSERT [dbo].[Compra_Producto] OFF
---
+++ src/AppForSEII2526.API/Data/dbo.TipoProducto.data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[TipoProducto] ON
+INSERT INTO [dbo].[TipoProducto] ([ProductoId], [Nombre]) VALUES (1, N'Camiseta')
+INSERT INTO [dbo].[TipoProducto] ([ProductoId], [Nombre]) VALUES (2, N'Sudadera')
+INSERT INTO [dbo].[TipoProducto] ([ProductoId], [Nombre]) VALUES (3, N'Gorra')
+SET IDENTITY_INSERT [dbo].[TipoProducto] OFF
