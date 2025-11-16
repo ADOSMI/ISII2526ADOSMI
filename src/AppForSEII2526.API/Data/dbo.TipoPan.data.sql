fa4e37c084de9abe9c3a36e1a14d0adbad714ffb
OscarMerino04 <oscar.merino2@alu.uclm.es>
2025-11-16 19:09:40 +0100

    Subidas las pruebas del Select y algún cambio menor en el DTO del Select para un correcto funcionamiento.

--- src/AppForSEII2526.API/Data/dbo.TipoPan.data.sql
+++
@@ -1,8 +0,0 @@
-SET IDENTITY_INSERT [dbo].[TipoPan] ON
-INSERT INTO [dbo].[TipoPan] ([Id], [Nombre]) VALUES (1, N'Rustico')
-INSERT INTO [dbo].[TipoPan] ([Id], [Nombre]) VALUES (2, N'Pipas')
-INSERT INTO [dbo].[TipoPan] ([Id], [Nombre]) VALUES (3, N'Integral')
-INSERT INTO [dbo].[TipoPan] ([Id], [Nombre]) VALUES (5, N'Semillas')
-INSERT INTO [dbo].[TipoPan] ([Id], [Nombre]) VALUES (6, N'Sin gluten')
-INSERT INTO [dbo].[TipoPan] ([Id], [Nombre]) VALUES (7, N'Normal')
-SET IDENTITY_INSERT [dbo].[TipoPan] OFF
