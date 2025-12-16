a7147601d3a3d75e2247dbfeae55ddbca45b0939
AlbertoCuencaAleman <lanterotz@gmail.com>
2025-12-16 21:19:10 +0100

    Cambios en el UIT

--- test/AppForSEII2526.UIT/CU_PedirBocadillos/UC_ComprarBocadillos_UIT.cs
+++ test/AppForSEII2526.UIT/CU_PedirBocadillos/UC_ComprarBocadillos_UIT.cs
@@ -134,7 +134,7 @@ namespace AppForSEII2526.UIT.UC_PedirBocadillo
 
             // Guardar y Confirmar Modal
             createCompra_PO.ClickGuardar();
-            detailCompraBocadillo_PO.WaitForDetailsPage();
+            detailCompraBocadillo_PO.WaitForDetailsPage();º
 
             // Verificamos URL
             Assert.Contains("detailcompra", _driver.Url);
